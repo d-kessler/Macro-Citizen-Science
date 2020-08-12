ab4e72a71c3079e87a71c356625e5a1e46f31e1d
d-kessler <67592365+d-kessler@users.noreply.github.com>
2020-08-11 22:50:18 -0400

    added crop

--- metadata_and_resizing.py
+++ metadata_and_resizing.py
@@ -8,21 +8,15 @@ from PIL import Image, ExifTags
 from pathlib import Path
 from datetime import datetime
 from panoptes_client import Panoptes, Project, SubjectSet, Subject
+import image_slicer
 
 
 def configure():
     """Get file & slab information, create new folder for images, configure excel & csv files"""
-    global subject_set_id, image_folder_path, excel_file_path, warehouse_name, location, granite_type, slab_id, \
-        resized_folder_path, csv_file_name, csv_file_path, metadata_fields, wb, ws, first_empty_row
-    
-    subject_set_id = 86450
+    global subject_set_id, image_folder_path, excel_file_path, warehouse_name, location, granite_type, slab_id, should_crop_into_four,\
+        cropped_folder_path, resized_folder_path, csv_file_name, csv_file_path, metadata_fields, wb, ws, first_empty_row
 
-    image_folder_path = r"C:\Users\dkess\OneDrive\Documents\CWRU\Macro\Data, Analysis\Slab 3 6x8 no flash"
-    excel_file_path = r"C:\Users\dkess\OneDrive\Documents\CWRU\Macro\Data, Analysis\Experiment_Manifest.xlsx"
-    warehouse_name = 'United Stone International'
-    location = 'Solon, Ohio'
-    granite_type = 'Dallas White'
-    slab_id = '1151|20'
+    subject_set_id = 86450
 
     # # getting user input for metadata fields
     # image_folder_path = input('Enter the folder path: ')
@@ -31,9 +25,24 @@ def configure():
     # location = input('Location (City, State): ')
     # granite_type = input('Granite type: ')
     # slab_id = input('Slab ID: ')
+    should_crop_into_four = input('Should the images be cropped into 4 parts? Enter \'yes\' or \'no\' :  ')
+
+    image_folder_path = r"C:\Users\dkess\OneDrive\Documents\CWRU\Macro\Data, Analysis\Slab 1 12x16 no flash - Copy"
+    excel_file_path = r"C:\Users\dkess\OneDrive\Documents\CWRU\Macro\Data, Analysis\Experiment_Manifest.xlsx"
+    warehouse_name = 'United Stone International'
+    location = 'Solon, Ohio'
+    granite_type = 'Dallas White'
+    slab_id = '1151|20'
+
+    # creating a folder for cropped images
+    cropped_folder_path = os.path.join(image_folder_path, r"cropped_" + os.path.basename(image_folder_path))
+    try:
+        os.mkdir(cropped_folder_path)
+    except:
+        print('Cropped Folder Exists.')
 
     # creating a folder for resized images
-    resized_folder_path = os.path.join(image_folder_path, r"resized-" + os.path.basename(image_folder_path))
+    resized_folder_path = os.path.join(image_folder_path, r"resized_" + os.path.basename(image_folder_path))
     try:
         os.mkdir(resized_folder_path)
     except:
@@ -52,7 +61,8 @@ def configure():
     csv_file_path = os.path.join(resized_folder_path, csv_file_name)
 
     with open(csv_file_path, 'w', newline='') as f:
-        metadata_fields = ['!subject_id', '#file_name', '#warehouse', '#location', '#granite_type', '#slab_id', '#date_time',
+        metadata_fields = ['!subject_id', '#file_name', '#warehouse', '#location', '#granite_type', '#slab_id',
+                           '#date_time',
                            '#latitude_longitude']
         csv_writer = csv.DictWriter(f, fieldnames=metadata_fields)
         csv_writer.writeheader()
@@ -68,6 +78,99 @@ def get_file_names():
         if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".png"):
             file_names.append(file)
 
+
+def crop_into_four(pil_file_):
+    global cropped_file_paths, resized_file_paths
+
+    cropped_file_paths = []
+    resized_file_paths = []
+
+    width, height = pil_file_.size
+
+    half_width = width / 2
+    half_height = height / 2
+
+    # starting from top left (0,0) and moving clockwise
+    # (left, top, right, bottom)
+    section_1 = (0, 0, half_width, half_height)
+    section_2 = (half_width, 0, width, half_height)
+    section_3 = (half_width, half_height, width, height)
+    section_4 = (0, half_height, half_width, height)
+
+    for i in range(1, 5):
+        im = pil_file_
+
+        if i == 1:
+            im = im.crop(section_1)
+        if i == 2:
+            im = im.crop(section_2)
+        if i == 3:
+            im = im.crop(section_3)
+        if i == 4:
+            im = im.crop(section_4)
+
+
+        reformatted_cropped_file_path = cropped_file_path.replace(str(extension), "_{}{}".format(str(i), str(extension)))
+        reformatted_resized_file_path = resized_file_path.replace(str(extension), "_{}{}".format(str(i), str(extension)))
+
+        cropped_file_paths.append(reformatted_cropped_file_path)
+        resized_file_paths.append(reformatted_resized_file_path)
+
+        im = im.save(reformatted_cropped_file_path, exif=image_exif)
+
+
+def resize_to_limit(image_file_path_=None, resized_file_path_=None, size_limit=600000):
+    """Resize images to size_limit, save to new file"""
+    # add conditional for should_resize
+
+    if should_crop_into_four == 'yes':
+
+        for cropped_file in cropped_file_paths:
+            index = list.index(cropped_file_paths, cropped_file)
+
+            img = Image.open(cropped_file)
+            aspect = img.size[0] / img.size[1]
+
+            while True:
+                with io.BytesIO() as buffer:
+                    img.save(buffer, format="JPEG")
+                    data = buffer.getvalue()
+                filesize = len(data)
+                size_deviation = filesize / size_limit
+                print("size: {}; factor: {:.3f}".format(filesize, size_deviation))
+
+                if size_deviation <= 1:
+                    img.save(resized_file_paths[index], exif=image_exif)
+                    print(cropped_file + '\nsaved to\n' + resized_file_paths[index])
+                    # print(img.size[0], img.size[1])
+                    break
+                else:
+                    new_width = img.size[0] / (1 * (size_deviation ** 0.5))
+                    new_height = new_width / aspect
+
+                    img = img.resize((int(new_width), int(new_height)))
+    else:
+        img = img_orig = Image.open(image_file_path_)
+        img_exif = img.info['exif']
+        aspect = img.size[0] / img.size[1]
+
+        while True:
+            with io.BytesIO() as buffer:
+                img.save(buffer, format="JPEG")
+                data = buffer.getvalue()
+            filesize = len(data)
+            size_deviation = filesize / size_limit
+            # print("size: {}; factor: {:.3f}".format(filesize, size_deviation))
+
+            if size_deviation <= 1:
+                img.save(resized_file_path_, exif=img_exif)
+                # print(img.size[0], img.size[1])
+                break
+            else:
+                new_width = img.size[0] / (1 * (size_deviation ** 0.5))
+                new_height = new_width / aspect
+
+                img = img_orig.resize((int(new_width), int(new_height)))
 
 def get_date(exif_dict):
     """Get date/time image exif data (if available)"""
@@ -140,33 +243,7 @@ def convert_to_degrees(value):
     return d + (m / 60.0) + (s / 3600.0)
 
 
-def resize_to_limit(image_file_path_, resized_file_path_, size_limit=600000):
-    """Resize images to size_limit, save to new file"""
-    img = img_orig = Image.open(image_file_path_)
-    img_exif = img.info['exif']
-    aspect = img.size[0] / img.size[1]
-
-    while True:
-        with io.BytesIO() as buffer:
-            img.save(buffer, format="JPEG")
-            data = buffer.getvalue()
-        filesize = len(data)
-        size_deviation = filesize / size_limit
-        # print("size: {}; factor: {:.3f}".format(filesize, size_deviation))
-
-        if size_deviation <= 1:
-            img.save(resized_file_path_, exif=img_exif)
-            # print(img.size[0], img.size[1])
-            break
-        else:
-            new_width = img.size[0] / (1 * (size_deviation ** 0.5))
-            new_height = new_width / aspect
-
-            img = img_orig.resize((int(new_width), int(new_height)))
-
-
 def write_metadata_into_excel():
-
     ws.cell(row=i, column=1).value = subject_id
     ws.cell(row=i, column=2).value = str(resized_file_name)
     ws.cell(row=i, column=3).value = warehouse_name
@@ -184,7 +261,6 @@ def write_metadata_into_excel():
 
 
 def write_metadata_into_csv():
-
     with open(csv_file_path, 'a', newline='') as f:
         csv_writer = csv.DictWriter(f, fieldnames=metadata_fields)
 
@@ -209,24 +285,32 @@ def write_metadata_into_csv():
 configure()
 get_file_names()
 
-# resize images, get and fill metadata into excel file & csv
+# crop (if applicable) and resize images, get and fill metadata into excel file & csv
 i = first_empty_row
 for filename in file_names:
-
     subject_id = 'e' + str(i - 1)
+    extension = os.path.splitext(filename)[-1]
+
     resized_file_name = r"res-" + filename
 
     image_file_path = os.path.join(image_folder_path, filename)
+    cropped_file_path = os.path.join(cropped_folder_path, filename)
     resized_file_path = os.path.join(resized_folder_path, resized_file_name)
 
-    resize_to_limit(image_file_path, resized_file_path)
+    # creating PIL instance
+    pil_file = Image.open(image_file_path)
+    image_exif = pil_file.info['exif']
+
+    if should_crop_into_four == 'yes':
+        crop_into_four(pil_file)
+        resize_to_limit()
+    else:
+        resize_to_limit(image_file_path, resized_file_path)
 
     # getting image exif data
-    pil_file = Image.open(image_file_path)
     exif = {ExifTags.TAGS[k]: v for k, v in pil_file._getexif().items() if k in ExifTags.TAGS}
 
     get_date(exif)
-
     get_gps(exif)
 
     write_metadata_into_excel()
